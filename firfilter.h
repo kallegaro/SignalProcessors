1aef3807dc5f88ba556601abd69bcf890fbd07cf
Bruno Callegaro <kallegaro@hotmail.com>
2016-05-04 00:06:50 -0300

    [NEW] Added some descriptions for the methods.

--- firfilter.h
+++ firfilter.h
@@ -13,14 +13,39 @@ class FIRFilter
 public:
     FIRFilter( ) = delete;
 
+    /**
+     * @brief FIRFilter
+     * Construct an FIR filter with the number of "taps" defined by the filter size.
+     * The coeficcients need to be configured with the method insertFilterCoef, otherwise,
+     * the filter will not be valid and will always return zero.
+     * @param filterSize
+     */
     FIRFilter( int filterSize );
 
+    /**
+     * @brief addElementAndProcess
+     * Inser an new element to the buffer array and then calculate the filter value. It will only
+     * calculate an valid output if the filter coefficients were correctly loaded.
+     * @param a
+     * @return
+     */
     Targ addElementAndProcess( const Targ & a );
 
+    /**
+     * @brief getVector
+     * Just an "get" method for returning the deque containing the last filterSize values.
+     * @return
+     */
     const std::deque< Targ > & getVector() const {
         return m_inputArray;
     }
 
+    /**
+     * @brief insertFilterCoef
+     * Insert an filter coefficients vector. The size will be checked and must be equal to filterSize,
+     * otherwise, the filter will awawys return zero.
+     * @param coef
+     */
     void insertFilterCoef( const std::vector<float> & coef ) {
         *m_coefArray = coef;
 
@@ -30,8 +55,8 @@ public:
     }
 
 private:
-    std::unique_ptr < std::deque< Targ > > m_inputArray;
-    std::unique_ptr < std::vector< Targ > > m_coefArray;
+    std::unique_ptr < std::deque< Targ > > m_inputArray;        /** An "linked-list", given that elements will be put and removed constantly. The "front" element is the newest one. */
+    std::unique_ptr < std::vector< Targ > > m_coefArray;        /** Given that the coef doesn't change once that they were loaded, this doesn't need to be an linked-list. The front element is the coefficient for the newest input value. */
     Targ m_sum;
 
     bool m_coefArrayLoaded;
--- sinewavegenerator.h
+++ sinewavegenerator.h
@@ -7,8 +7,21 @@
 class SineWaveGenerator
 {
 public:
+
+    /**
+     * @brief SineWaveGenerator
+     * This will construct a class that will calculate the value of a sin wave for a determined time "t".
+     * @param frequency sin wave frequency in Hertz ( cycles per second )
+     * @param phase sin wave phase in Deegres
+     */
     explicit SineWaveGenerator( double frequency, double phase = 0.0f );
 
+    /**
+     * @brief getValue
+     * Calculate the sin wave value for a given time "t"
+     * @param t
+     * @return
+     */
     double getValue( double t );
 
 private:
